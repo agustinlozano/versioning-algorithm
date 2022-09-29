93f3a80e89c36b572d9b499c8c8fad43563e60d1
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-09-29 16:43:09 -0300

    Edit my ugly Go code :D

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
+// Given an array of strings and a plain text file
+// make a function to check the matching of the strings in the file
+// and return the number of matches
+
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"reflect"
+)
 
 func main() {
-    fmt.Println("Hello, world.")
+	// Define an array of strings with five elements
+	guids := []string{
+		"0dLSQoDLzFMBd4W2uDyJKF",
+		"1KBlFBdjz0Tf_GT2VxZ7_I",
+		"36zqNfIRfCvQpZuJcz2xU7",
+		"1KBlFBdjz0Tf_GUCtxZ7ti",
+		"0wQknZQiXEOBcv3sHGzx_O"}
+
+
+
+	// Open the file
+	myFile, err := os.Open("./IFCs/myIFC.ifc")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer myFile.Close()
+
+	checkMatches(myFile, guids)
+
+	fmt.Println(reflect.TypeOf(myFile))
+}
+
+func checkMatches(ifcModel &os.file, guids []string) int {
+	fmt.Println(guids)
+
+	// Read the file
+	scanner := bufio.NewScanner(ifcModel)
+	scanner.Split(bufio.ScanLines)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	// Close the file
+	ifcModel.Close()
+
+	// Check the matches
+	matches := 0
+	for _, line := range lines {
+		if strings.Contains(line, "Hello") {
+			matches++
+		}
+	}
+
+	// Print the number of matches
+	fmt.Println(matches)
+
+	return 0
 }
